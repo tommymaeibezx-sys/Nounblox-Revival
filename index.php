3a037b8487ab3dbc2b960f3149eeaf540087fbc3
tommymaeibezx-sys <tommymaeibezx@gmail.com>
2026-07-01 15:09:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 $viene_de_aspx = true;
 
 // Incluir index.php desde la raíz del proyecto
-include_once($_SERVER['DOCUMENT_ROOT'] . '/Games.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/Default.aspx');
 exit();
