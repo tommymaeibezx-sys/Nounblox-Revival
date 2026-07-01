5fc4b0a7993eacda5ec80f90bf95bf9142c6a63b
tommymaeibezx-sys <tommymaeibezx@gmail.com>
2026-07-01 14:40:25 -0400

    Update database connection credentials

--- core/db.php
+++ core/db.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'mysql.railway.internal');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'mgsBCRPSiVumrurjUQiZhTuykZzYjvdc');
-define('DB_NAME', 'railway');
+define('DB_SERVER', 'sql5.freesqldatabase.com');
+define('DB_USERNAME', 'sql5832036');
+define('DB_PASSWORD', 'yQYHcDXZef');
+define('DB_NAME', 'sql5832036');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
